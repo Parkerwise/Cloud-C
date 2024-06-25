fb2fd09148922711635ba20ab2acb106bfd63b38
Parkerwise <wiseparker03@gmail.com>
2024-06-25 18:12:21 -0500

    added comments

--- spectral-analysis/sgrb2/get_molecules.cpp
+++ spectral-analysis/sgrb2/get_molecules.cpp
@@ -12,17 +12,22 @@
 #include <string>
 
 double GetFrequencies(std::string line){ 
+	//Values are specified by bytes in dat files
+	//so our string must be converted to bytes
 	std::vector<char> bytes(line.begin(), line.end());
 	bytes.push_back('\0');
+	//Frequencies are from byte 61-70
 	std::string FrequencyString(bytes.begin()+60,bytes.begin()+70);
 	double Frequency = std::stod(FrequencyString,0);
 	return Frequency;
 }
+//The same process is repeated to get transitions (bytes 1-21)
 std::string GetTransitions(std::string line){
 	std::vector<char> bytes(line.begin(), line.end());
 	bytes.push_back('\0');
 	std::string Transition(bytes.begin(),bytes.begin()+21);
-	std::string Molecule = Transition.substr(0, Transition.find(",", 0));
+	//removes vibrational states since they are specified after a comma
+	std::string Molecule = Transition.substr(0, Transition.find(",", 0)); 
 	Molecule.erase(std::remove_if(Molecule.begin(), Molecule.end(), isspace), Molecule.end());
 	return Molecule;
 }
@@ -44,21 +49,23 @@ if (!MoleculeList.is_open()) {
 	std::cerr << "Error opening the file!" << std::endl; 
 	return 1;
 	}
-while (getline(MoleculeList,line)){
+while (getline(MoleculeList,line)){ //populates vector with molecules in freq range
 	float IndidualFrequency=GetFrequencies(line);
 	if (IndidualFrequency > StartFreq && IndidualFrequency < EndFreq){
 	std::string IndividualMolecule=GetTransitions(line);
 	AllMolecules.push_back(IndividualMolecule);
 	}
+//Sort and erase remove duplicates (different transitions, same molecule)
 sort(AllMolecules.begin(),AllMolecules.end());
 AllMolecules.erase(std::unique(AllMolecules.begin(),AllMolecules.end()),AllMolecules.end());
 }
 MoleculeList.close();
+//outputs string formatted as a python list for use in xclass
 std::string SelectedMolecules = "[";
 std::string FinalMolecule=AllMolecules.back();
 for (std::string i : AllMolecules){
 	SelectedMolecules.append(i);
-	if (i != FinalMolecule){
+	if (i != FinalMolecule){ //avoids comma after final entry
 	SelectedMolecules.append(",");
 	}
 }
