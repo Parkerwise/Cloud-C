9e4f47e5884c9f059f7b186e7004c2e030f2af8c
Parkerwise <wiseparker03@gmail.com>
2024-08-09 11:45:13 -0400

    Spectra towards all regions in line rich cubes

--- hot-cores.py
+++ hot-cores.py
@@ -18,57 +18,66 @@ if not sys.warnoptions:
 
 # Reading in files
 # # This should be the file where we're extracting spectra
-path = "/home/pw/research/Cloud-C/co-data/B.Dust_Ridge_sci.spw31.cube.I.pbcor.fits"
-# # creates spectral cube object
-sc = SpectralCube.read(path)
-sc.allow_huge_operations = True
+cubeList = [
+    'A.Dust_Ridge_sci.spw29.cube.I.pbcor.fits',  # A29
+    'A.Dust_Ridge_sci.spw31.cube.I.pbcor.fits',  # A31
+    'B.Dust_Ridge_sci.spw29.cube.I.pbcor.fits',  # B29
+    'B.Dust_Ridge_sci.spw31.cube.I.pbcor.fits',  # B31
+]
+cubeAbbreviation = [
+    "A.spw29",  # A29
+    "A.spw31",  # A31
+    "B.spw29",  # B29
+    "B.spw31",  # B31
+]
+for cube, name in zip(cubeList, cubeAbbreviation):
+    path = f"/home/pw/research/Cloud-C/co-data/{cube}"
+    # # creates spectral cube object
+    sc = SpectralCube.read(path)
+    sc.allow_huge_operations = True
 
+    # Converts units to GHz and K, feel free to change
+    # #  defaults should be Hz and Jy/Beam
+    sc = sc.with_spectral_unit(u.GHz)
+    sc = sc.to(u.K)
+    # # defines our frequency as a list
+    freq, Dec, Ra = sc.world[:, 0, 0]
 
-# Converts units to GHz and K, feel free to change
-# #  defaults should be Hz and Jy/Beam
-sc = sc.with_spectral_unit(u.GHz)
-sc = sc.to(u.K)
-# # defines our frequency as a list
-freq, Dec, Ra = sc.world[:, 0, 0]
+    # defines a subcube around your source
+    # # From regions file
 
-
-# defines a subcube around your source
-# # From regions file
-
-regions_file = "/home/pw/research/Cloud-C/fk5Regions.reg"
-regpix = Regions.read(regions_file)
-numberOfRegions = len(regpix)
-# averages values within aperture for each channel
-'''
-Noise_upper = 825  # defines some line free channels to calculate STD
-Noise_lower = 800
-'''
-
-fig1 = plt.figure(1, figsize=(15, 2*numberOfRegions), dpi=250)
-for i in range(numberOfRegions):
-    subcube = sc.subcube_from_regions([regpix[i]])
-    spectrum = subcube.mean(axis=(1, 2))
-    ax1 = plt.subplot(numberOfRegions, 1, i+1)
-    ax1.plot(freq, spectrum, lw=1, drawstyle='steps-mid', color="SteelBlue",
-             label="spectrum")
-    ax1.set_title(f"region {i}")
+    regions_file = "/home/pw/research/Cloud-C/fk5Regions.reg"
+    regpix = Regions.read(regions_file)
+    numberOfRegions = len(regpix)
+    # averages values within aperture for each channel
     '''
-    sigma = np.std(spectrum[Noise_lower:Noise_upper].value)
-    three_sigma = 3*sigma
-    plt.hlines(three_sigma, freq[0].value, freq[1916].value, colors="red",
-               label='', ls="--")
-    plt.hlines(-three_sigma, freq[0].value, freq[1916].value, colors="red",
-               label='', ls="--")
-    plt.xlim(freq[0].value, freq[1916].value)
-    plt.fill_between(freq.value, three_sigma, -three_sigma, alpha=0.2,
-                     color='red', label='Error')
+    Noise_upper = 825  # defines some line free channels to calculate STD
+    Noise_lower = 800
     '''
-fig1.supxlabel("Frequency (GHz)", fontsize=10)
-fig1.supylabel('Brightness Temp. (K)', fontsize=10)
-plt.legend(fontsize=14, loc="upper left")
-plt.tight_layout()  # Adjust params to avoid overlap and decrease white space
-# good to save as both png and pdf
-plt.savefig("/home/pw/research/Cloud-C/results/spectra/line-richness.png")
-plt.savefig("/home/pw/research/Cloud-C/results/spectra/line-richness.pdf")
-# Saving figures
 
+    fig1 = plt.figure(1, figsize=(15, 2*numberOfRegions), dpi=250)
+    for i in range(numberOfRegions):
+        subcube = sc.subcube_from_regions([regpix[i]])
+        spectrum = subcube.mean(axis=(1, 2))
+        ax1 = plt.subplot(numberOfRegions, 1, i+1)
+        ax1.plot(freq, spectrum, lw=1, drawstyle='steps-mid', color="SteelBlue",
+                 label="spectrum")
+        ax1.set_title(f"region {i}")
+        '''
+        sigma = np.std(spectrum[Noise_lower:Noise_upper].value)
+        three_sigma = 3*sigma
+        plt.hlines(three_sigma, freq[0].value, freq[1916].value, colors="red",
+                   label='', ls="--")
+        plt.hlines(-three_sigma, freq[0].value, freq[1916].value, colors="red",
+                   label='', ls="--")
+        plt.xlim(freq[0].value, freq[1916].value)
+        plt.fill_between(freq.value, three_sigma, -three_sigma, alpha=0.2,
+                         color='red', label='Error')
+        '''
+    fig1.supxlabel("Frequency (GHz)", fontsize=10)
+    fig1.supylabel('Brightness Temp. (K)', fontsize=10)
+    plt.legend(fontsize=14, loc="upper left")
+    plt.tight_layout()  # Adjust params to avoid overlap and decrease white space
+    # good to save as both png and pdf
+    plt.savefig(f"/home/pw/research/Cloud-C/results/spectra/{name}/{name}-line-richness.png")
+    plt.savefig(f"/home/pw/research/Cloud-C/results/spectra/{name}/{name}-line-richness.pdf")
